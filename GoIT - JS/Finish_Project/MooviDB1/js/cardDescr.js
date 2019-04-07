d40b5de783f5ad3ccac0ae898f36a455b441c97f
Vasia1101 <vasilij.gajda@gmail.com>
2019-04-07 08:13:39 +0300

    project_js_link_ready

--- GoIT - JS/Finish_Project/MooviDB1/js/cardDescr.js
+++ GoIT - JS/Finish_Project/MooviDB1/js/cardDescr.js
@@ -1,4 +1,88 @@
 "use strict";
+// function menu
+document.querySelector('main').addEventListener('click', () => {menuBtn.className = menuIconClosed;
+  menuContainer.className = menuContClosed;});
+const menuBtn = document.getElementById('js-menuBtn');
+const menuContainer = document.getElementById('js-menuContainer');
+
+const menuIconClosed = "menu-icon closed"; 
+const menuIconOpened = "menu-icon opened"; 
+const menuContClosed = "menu-container closed"; 
+const menuContOpened = "menu-container opened"; 
+
+menuBtn.addEventListener('click', function() {
+  if (menuBtn.className == menuIconClosed) {
+    menuBtn.className = menuIconOpened;
+    menuContainer.className = menuContOpened;
+  } else if (menuBtn.className == menuIconOpened) {
+    menuBtn.className = menuIconClosed;
+    menuContainer.className = menuContClosed;
+  }
+  });
+
+
+
+  // ----------------------------- Juli Modal ------------------------//
+;(function() {
+  function isValue(el, arr) {
+      for (let i = 0; i < arr.length; i++) {
+              if (el.value) {
+              el.classList.add("active");
+          } else {
+              el.classList.remove("active");
+          }
+      }
+  }
+  
+  let inputs = document.querySelectorAll(".js_input");
+  let newArr = Array.from(inputs);
+  let close = document.querySelector('.js_close_icon');
+  let form = document.querySelector('.js_form');
+  let formWrapper = document.querySelector('.js_container_wrapper');
+  let thanksMessageModal = document.querySelector('.js_thanks_message_modal');
+  let openModal = document.querySelectorAll('.js-open_modal');
+  let bntOkModal = document.querySelector('.js_submit_ok');
+
+  let formContent = document.querySelector('.js_form__content');
+
+  newArr.forEach(element => element.addEventListener("change", () => isValue(element, newArr)));
+
+  function sendForm() {
+      event.preventDefault();
+      showThanksMessage();
+  }
+
+  close.addEventListener('click', function() {
+      formWrapper.classList.add('close_modal');
+  });
+  bntOkModal.addEventListener('click', function() {
+      formWrapper.classList.add('close_modal');
+  });
+  
+  function showThanksMessage() {
+      thanksMessageModal.classList.add('active');
+      formContent.classList.add('hide');
+  }
+  
+  openModal.forEach(element => element.addEventListener('click', function() {
+      formWrapper.classList.remove('close_modal');
+      thanksMessageModal.classList.remove('active');
+      formContent.classList.remove('hide');
+      newArr.forEach(element => element.value = '');
+  }));
+
+  form.addEventListener('submit', sendForm);
+
+})();
+
+
+
+
+
+
+
+
+
 
 let title = document.querySelector(".card-descr_title");
 let titleDesktop = document.querySelector(".card-descr_titleDesktop");
